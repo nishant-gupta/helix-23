42e4977ec58cbcf1f8d6bc0806ffba193b15cea5
nishantbd <nishta.gupta@bd.com>
2023-03-16 18:55:16 +0530

    lint

--- blocks/countries/countries.js
+++ blocks/countries/countries.js
@@ -1,14 +1,12 @@
-//https://main--helix-23--nishant-gupta.hlx.live/lists/countries-list.json
+//  https://main--helix-23--nishant-gupta.hlx.live/lists/countries-list.json
 
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
- export default async function decorate(block) {
-    //block.textContent = '';
-  
-    //fetch nav content
-    //const navPath = getMetadata('nav') || '/nav';
+ export default async function decorate(block) {  
+    //  fetch nav content
+    //  const navPath = getMetadata('nav') || '/nav';
     const resp = await fetch(`/lists/countries-list.json`);
   
     if (resp.ok) {
@@ -17,7 +15,7 @@
         const ul = document.createElement('ul');
         json.data.forEach((row) => {
             const li = document.createElement('li');
-            li.innerHTML = row.Country + ' (' + row.Population + ')';          
+            li.innerHTML = row.Country + ' (' + row.Population + ')';
             ul.append(li);
         });
         block.append(ul);
