9d0c324c57a344a760c938c1a2d5b8f1286361f3
nishantbd <nishta.gupta@bd.com>
2023-03-17 11:46:07 +0530

    lint fix

--- blocks/countries/countries.js
+++ blocks/countries/countries.js
@@ -4,21 +4,21 @@
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
- export default async function decorate(block) {  
-    //  fetch nav content
-    //  const navPath = getMetadata('nav') || '/nav';
-    const resp = await fetch(`/lists/countries-list.json`);
-  
-    if (resp.ok) {
-      const json = await resp.json();
-        /* change to ul, li */
-        const ul = document.createElement('ul');
-        json.data.forEach((row) => {
-            const li = document.createElement('li');
-            li.innerHTML = row.Country + ' (' + row.Population + ')';
-            ul.append(li);
-        });
-        block.append(ul);
-    }
+export default async function decorate(block) {  
+  //  fetch nav content
+  //  const navPath = getMetadata('nav') || '/nav';
+  const resp = await fetch(`/lists/countries-list.json`);
+
+  if (resp.ok) {
+    const json = await resp.json();
+      /* change to ul, li */
+      const ul = document.createElement('ul');
+      json.data.forEach((row) => {
+          const li = document.createElement('li');
+          li.innerHTML = row.Country + ' (' + row.Population + ')';
+          ul.append(li);
+      });
+      block.append(ul);
   }
+}
   
